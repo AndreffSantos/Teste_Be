17b0e0022c13eae86177ff750c3825187bca0f1b
Andre Santos <andrefelipe_santos@outlook.com>
2024-07-02 09:02:52 -0300

    Cria rota "signup"

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,7 @@
 |
 */
 
+import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
@@ -14,3 +15,6 @@ router.get('/', async () => {
     hello: 'world',
   }
 })
+
+// User
+router.post('/signup', [UsersController, 'store'])
