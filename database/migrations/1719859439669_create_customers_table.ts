aafba4cf4683713f8cf746d53c4ef704eef41fc3
Andre Santos <andrefelipe_santos@outlook.com>
2024-07-02 16:46:28 -0300

    Fix: torna o campo cpf unico no banco de dados.

--- database/migrations/1719859439669_create_customers_table.ts
+++ database/migrations/1719859439669_create_customers_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
       table.string('name', 80).nullable()
-      table.string('cpf', 14).notNullable()
+      table.string('cpf', 14).notNullable().unique()
     })
   }
 
