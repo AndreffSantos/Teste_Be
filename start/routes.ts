98d53b13b1c05baa245da90736403463458c05ee
Andre Santos <andrefelipe_santos@outlook.com>
2024-07-03 16:35:33 -0300

    rotas para clientes

--- start/routes.ts
+++ start/routes.ts
@@ -7,8 +7,10 @@
 |
 */
 
+import CustomersController from '#controllers/customers_controller'
 import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
@@ -19,3 +21,10 @@ router.get('/', async () => {
 // User
 router.post('/signup', [UsersController, 'store'])
 router.post('/login', [UsersController, 'login'])
+
+// Customer
+router.get('/customers', [CustomersController, 'index']).use(middleware.auth())
+router.get('/customer/:id/:date?', [CustomersController, 'show']).use(middleware.auth())
+router.put('/customer/:id/', [CustomersController, 'update']).use(middleware.auth())
+router.post('/customers', [CustomersController, 'store']).use(middleware.auth())
+router.delete('/customer/:id', [CustomersController, 'destroy'])
