bd1b93ca3c78d040c9dc9b1e36cc0c5b5f111a55
Andre Santos <andrefelipe_santos@outlook.com>
2024-07-03 18:56:09 -0300

    Validação do requests de produto.

---
+++ app/validators/product.ts
@@ -0,0 +1,11 @@
+import vine from '@vinejs/vine'
+
+export const storeProductValidator = vine.compile(
+    vine.object({
+        name: vine.string(),
+        description: vine.string().optional(),
+        stock: vine.number().positive().optional(),
+        category: vine.string().optional(),
+        price: vine.number(),
+    })
+)
